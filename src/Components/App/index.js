3be54aa466f4049b7d58b9e835d1a06346101ee5
Shadrach Lagrimas <shadrachlagrimas@Shadrachs-MBP.home>
2018-11-16 12:34:12 -1000

    moved app into components folder

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <h3 className="some-class">This is a test</h3> 
           <a
             className="App-link"
             href="https://reactjs.org"
